d2e8a46157987e7bfeb585930ac983fb020bff8d
allef-noronha <118064640+allef-noronha@users.noreply.github.com>
2025-10-15 21:30:59 -0300

    Liberando acesso ao site

--- config/site.js
+++ config/site.js
@@ -1,6 +1,6 @@
 // /config/site.js
 window.SITE = {
-  open: false, // você pode deixar false até o dia
+  open: true, // você pode deixar false até o dia
   launchAt: '2025-10-22T00:00:00-03:00', // ajuste o horário se quiser
 
   whitelist: [
