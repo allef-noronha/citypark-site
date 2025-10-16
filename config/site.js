74c49638e352db28b2ef4265dc2e7b171ff48c73
allef-noronha <118064640+allef-noronha@users.noreply.github.com>
2025-10-15 21:42:47 -0300

    Travando a liberação do site.

--- config/site.js
+++ config/site.js
@@ -1,7 +1,7 @@
 // /config/site.js
 window.SITE = {
   open: false, // você pode deixar false até o dia
-  launchAt: '2025-10-15T00:00:00-03:00', // ajuste o horário se quiser
+  launchAt: '2025-10-22T00:00:00-03:00', // ajuste o horário se quiser
 
   whitelist: [
     'aguarde.html',
